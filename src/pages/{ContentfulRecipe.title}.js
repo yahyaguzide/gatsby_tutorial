83961e8f3b645d06fec5ed5b825ceefed19b21ec
yahyaguzide <yahyaguzide@hotmail.de>
2023-05-01 12:45:47 +0200

    finished dynamic page creation with Gatsby and Graphql

--- src/pages/{ContentfulRecipe.title}.js
+++ src/pages/{ContentfulRecipe.title}.js
@@ -1,24 +1,132 @@
-import { graphql, useStaticQuery } from 'gatsby'
-import React from 'react'
+import * as React from "react";
+import { graphql, Link } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import { BsClockHistory, BsClock, BsPeople } from "react-icons/bs";
+import Layout from "../components/Layout";
 
-const query = graphql`
+export const query = graphql`
   query getSingleRecipe($id: String) {
     contentfulRecipe(id: { eq: $id }) {
       title
       cookTime
+      content {
+        ingredients
+        instructions
+        tags
+        tools
+      }
+      description {
+        description
+      }
+      prepTime
+      servings
+      image {
+        gatsbyImageData(layout: CONSTRAINED, placeholder: DOMINANT_COLOR)
+      }
     }
   }
-`
+`;
 
-const RecipeTemplate = () => {
+const RecipeTemplate = ({ data }) => {
   const {
-    contentfulRecipe: { title, cookTime }
-  } = useStaticQuery(query)
+    title,
+    cookTime,
+    content,
+    prepTime,
+    servings,
+    description: { description },
+    image,
+  } = data.contentfulRecipe;
+  const pathToImage = getImage(image);
+  const { tags, instructions, ingredients, tools } = content;
   return (
-    <div>
-      {title} {cookTime}
-    </div>
-  )
-}
+    <Layout>
+      <main className="page">
+        <div className="recipe-page">
+          {/* hero */}
+          <section className="recipe-hero">
+            <GatsbyImage
+              image={pathToImage}
+              alt={title}
+              className="about-image"
+            />
+            <article className="recipe-info">
+              <h2>{title}</h2>
+              <p>{description}</p>
+              {/* icons */}
+              <div className="recipe-icons">
+                <article>
+                  <BsClock />
+                  <h5>prep Time</h5>
+                  <p>{prepTime} min.</p>
+                </article>
+                <article>
+                  <BsClockHistory />
+                  <h5>cook Time</h5>
+                  <p>{cookTime} min.</p>
+                </article>
+                <article>
+                  <BsPeople />
+                  <h5>Servings</h5>
+                  <p>{servings}</p>
+                </article>
+              </div>
+              {/* Tags */}
+              <p className="recipe-tags">
+                Tags:
+                {tags.map((tag, index) => {
+                  return (
+                    <Link to={"/${tag}"} key={index}>
+                      {tag}
+                    </Link>
+                  );
+                })}
+              </p>
+            </article>
+          </section>
+          {/* rest of the Content */}
+          <section className="recipe-content">
+            <article>
+              <h4>instructions</h4>
+              {instructions.map((item, index) => {
+                return (
+                  <div key={index} className="single-instruction">
+                    <header>
+                      <p>step {index + 1}</p>
+                      <div></div>
+                    </header>
+                    <p>{item}</p>
+                  </div>
+                );
+              })}
+            </article>
+            <article className="second-column">
+              <div>
+                <h4>ingredients</h4>
+                {ingredients.map((item, index) => {
+                  return (
+                    <p key={index} className="single-ingredient">
+                      {item}
+                    </p>
+                  );
+                })}
+              </div>
+              <div>
+                <h4>tools</h4>
+                {tools.map((item, index) => {
+                  return (
+                    <p key={index} className="single-tool">
+                      {item}
+                    </p>
+                  );
+                })}
+              </div>
+            </article>
+          </section>
+        </div>
+      </main>
+    </Layout>
+  );
+};
 
-export default RecipeTemplate
+export default RecipeTemplate;
